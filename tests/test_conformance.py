677038ff86022d1e7cbd95597436bd330466fe3d
PND <fujjisaaan@gmail.com>
2019-07-26 17:14:57 +0900

    Fix the test code.
    Arguments passed to `pytest.mark.parametrize` were incorrect.

--- tests/test_conformance.py
+++ tests/test_conformance.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 import pytest
 import yaml
@@ -11,8 +11,14 @@ def read_yaml(path) -> dict:
         return yaml.safe_load(f)
 
 
-def get_table(test_cases: dict, group_name: str) -> List[list]:
-    return [list(case.values()) for case in test_cases[group_name]]
+def get_table(test_cases: dict, group_name: str) -> Tuple[str, List[list]]:
+    header = ",".join(test_cases[group_name][0].keys())
+    values = [list(case.values()) for case in test_cases[group_name]]
+    return header, values
+
+
+def parametrize(test_cases: dict, group_name: str):
+    return pytest.mark.parametrize(*get_table(test_cases, group_name))
 
 
 extract = read_yaml('tests/cases/extract.yml')['tests']
@@ -20,57 +26,36 @@ tlds = read_yaml('tests/cases/tlds.yml')['tests']
 validate = read_yaml('tests/cases/validate.yml')['tests']
 
 
-@pytest.mark.parametrize(
-    'description,text,expected',
-    get_table(extract, 'tco_urls_with_params')
-)
+@parametrize(extract, 'tco_urls_with_params')
 def test_extract_tco_urls_with_params(description: str, text: str, expected: List[str]):
     assert extract_urls(text) == expected
 
 
-@pytest.mark.parametrize(
-    'description,text,expected',
-    get_table(extract, 'urls')
-)
+@parametrize(extract, 'urls')
 def test_extract_urls(description: str, text: str, expected: List[str]):
     assert extract_urls(text) == expected
 
 
-@pytest.mark.parametrize(
-    'description,text,expected',
-    get_table(tlds, 'country')
-)
+@parametrize(tlds, 'country')
 def test_tlds_country(description: str, text: str, expected: List[str]):
     assert extract_urls(text) == expected
 
 
-@pytest.mark.parametrize(
-    'description,text,expected',
-    get_table(extract, 'urls_with_indices')
-)
+@parametrize(extract, 'urls_with_indices')
 def test_extract_urls_with_indices(description: str, text: str, expected: dict):
     assert extract_urls_with_indices(text) == expected
 
 
-@pytest.mark.parametrize(
-    'description,text,expected',
-    get_table(extract, 'urls_with_directional_markers')
-)
+@parametrize(extract, 'urls_with_directional_markers')
 def test_extract_urls_with_directional_markers(description: str, text: str, expected: dict):
     assert extract_urls_with_indices(text) == expected
 
 
-@pytest.mark.parametrize(
-    'description,text,expected',
-    get_table(validate, 'WeightedTweetsWithDiscountedEmojiCounterTest')
-)
+@parametrize(validate, 'WeightedTweetsWithDiscountedEmojiCounterTest')
 def test_validate_weighted_tweets_with_discounted_emoji_counter_test(description: str, text: str, expected: dict):
     assert parse_tweet(text) == expected
 
 
-@pytest.mark.parametrize(
-    'description,text,expected',
-    get_table(validate, 'UnicodeDirectionalMarkerCounterTest')
-)
+@parametrize(validate, 'UnicodeDirectionalMarkerCounterTest')
 def test_validate_unicode_directional_marker_counter_test(description: str, text: str, expected: dict):
     assert parse_tweet(text) == expected
